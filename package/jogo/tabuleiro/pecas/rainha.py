79fedc38fe4f6d76c53d39cb55e83fd07c9e0367
MarcosBritoSAR <marcosbryto92@gmail.com>
2024-06-03 12:00:02 -0300

    concluindo rainha e rei

--- package/jogo/tabuleiro/pecas/rainha.py
+++ package/jogo/tabuleiro/pecas/rainha.py
@@ -1,20 +1,43 @@
 from pecas_method_protocol import Pecas_protocol
 
 class Rainha(Pecas_protocol):
-<<<<<<< HEAD
 
     def __init__(self) -> None:
         super().__init__()
 
     def movimento(pos_inicial: Casa) -> List:
-        return super().movimento()
+
+        movimentos_possiveis = []
+
+        for i in range(8):
+            if i != x:
+                movimentos_possiveis.append((i, y))
+
+        for j in range(8):
+            if j != y:
+                movimentos_possiveis.append((x, j))
+
+ 
+        for i in range(1, 8):
+
+            if x + i < 8 and y + i < 8:
+                movimentos_possiveis.append((x + i, y + i))
+
+            if x + i < 8 and y - i >= 0:
+                movimentos_possiveis.append((x + i, y - i))
+
+            if x - i >= 0 and y + i < 8:
+                movimentos_possiveis.append((x - i, y + i))
+        
+            if x - i >= 0 and y - i >= 0:
+                movimentos_possiveis.append((x - i, y - i))
+
+        return movimentos_possiveis
+
     
     def is_valido(list: Casa) -> bool:
-        return super().is_valido()
-=======
-    
-    def movimento(pos_inicial: Casa) -> List :
-        pass
-    def is_valido(list: Casa) -> bool :
-        pass
->>>>>>> 12df93ca56ba8af45a1a118b0f977f387a8e32a0
+        for i in range(len(casas)):
+            for verifica in cassa[i]:
+                if not verifica.get_pecas() is None:
+                    return false
+        return true
--- package/jogo/tabuleiro/pecas/torre.py
+++ package/jogo/tabuleiro/pecas/torre.py
@@ -1,12 +1,34 @@
 from pecas_method_protocol import Pecas_protocol
-class Cavalo(Pecas_protocol):
+class Torre(Pecas_protocol):
     
     def __init__(self) -> None:
         self._first_play_torre = True
     
     def movimento(pos_inicial:Casa) -> List:
-        self._first_play_torre = False
-        pass
+        
+        self._fist_play_king = False
+        x = pos_inicial.x()
+        y = pos_inicial.y()
+
+        movimentos_possiveis = []
+        movimentos_possiveis = []
+
+        for i in range(8):
+            if i != x:
+                movimentos_possiveis.append((i, y))
+
+        
+        for j in range(8):
+            if j != y:
+                movimentos_possiveis.append((x, j))
+        return movimentos_possiveis
+
+    def is_valido(seld, casas: Casa) -> bool:
+        for i in range(len(casas)):
+            for verifica in cassa[i]:
+                if not verifica.get_pecas() is None:
+                    return false
+        return true
 
     def first_play_torre(self):
         return self._first_play_torre
