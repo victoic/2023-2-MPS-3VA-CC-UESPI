c32f14d11e4823d7407b2f5f895a76ab59bfda6a
MarcosBritoSAR <marcosbryto92@gmail.com>
2024-06-03 11:24:23 -0300

    create roque of king

--- package/jogo/tabuleiro/pecas/rei.py
+++ package/jogo/tabuleiro/pecas/rei.py
@@ -1,14 +1,33 @@
 from typing import List
 from pecas_method_protocol import Pecas_protocol
+import sys
+import os
 from casa import Casa
+from torre import Torre
+sys.path.append(os.path.abspath( '../casa'))
+sys.path.append(os.path.abspath( '../torre'))
+
+
 
 class Rei(Pecas_protocol):
 
-    def __init__(self, roque_feito : bool):
-        self.roque_feito = roque_feito
+    def __init__(self, ):
+        self._roque_feito = None
+        self._first_play_king = True
+
         
     def movimento(pos_inicial: Casa) -> List:
-        return super().movimento()
+        self._fist_play_king = False
+        
     
     def is_valido(list: Casa) -> bool:
-        return super().is_valido()
+        pass
+
+    def verificaTorre_isValid(self, torre : Torre) -> bool:
+        return torre.first_play_torre()
+
+    def is_safe(self) -> bool:
+        pass
+
+    def is_roque_valid(self):
+        return self._first_play_king and self.verificaTorre_isValid() and self.is_safe()
--- package/jogo/tabuleiro/pecas/torre.py
+++ package/jogo/tabuleiro/pecas/torre.py
@@ -2,8 +2,12 @@ from pecas_method_protocol import Pecas_protocol
 class Cavalo(Pecas_protocol):
     
     def __init__(self) -> None:
-        pass
+        self._first_play_torre = True
     
     def movimento(pos_inicial:Casa) -> List:
+        self._first_play_torre = False
         pass
+
+    def first_play_torre(self):
+        return self._first_play_torre
     
