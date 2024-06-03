bb5c831ba15ac2af57d8e79a48cd19e7d73a7e6b
Mateus Vieira <mateusmvsud@hotmail.com>
2024-06-03 10:48:23 -0300

    atualizando def __init__ da classe Peca

--- package/jogo/tabuleiro/pecas/__init__.py
+++ package/jogo/tabuleiro/pecas/__init__.py
@@ -2,8 +2,9 @@ from casa import Casa
 from abc import ABC, abstractmethod
 
 class Peca:
-    def __init__(self):
-        pass
+    def __init__(self, is_branca):
+        self.is_branca = is_branca
+        self.is_morta = False
 
     def is_branca(self, peca):
         if(peca == True):
