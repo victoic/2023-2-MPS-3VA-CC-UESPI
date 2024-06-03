12df93ca56ba8af45a1a118b0f977f387a8e32a0
MarcosBritoSAR <marcosbryto92@gmail.com>
2024-06-03 10:11:43 -0300

    banch pecas

---
+++ package/jogo/tabuleiro/pecas/rainha.py
@@ -0,0 +1,8 @@
+from pecas_method_protocol import Pecas_protocol
+
+class Rainha(Pecas_protocol):
+    
+    def movimento(pos_inicial: Casa) -> List :
+        pass
+    def is_valido(list: Casa) -> bool :
+        pass
