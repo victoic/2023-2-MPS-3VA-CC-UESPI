b25bd3d14a331ba64f4c2fdb3d6311c4e463a89b
MarcosBritoSAR <marcosbryto92@gmail.com>
2024-06-03 09:15:01 -0300

    Estruturação das peças e criação da classe Piao

---
+++ package/jogo/tabuleiro/pecas/piao.py
@@ -0,0 +1,13 @@
+from typing import List, Any
+class Piao():
+    def __init__(self) -> None:
+         pass
+     
+     
+     #Any será substituido Casa
+    def movimentos(self, pos_inicial : Any) -> List:
+         pass
+    
+     #Any será substituido Casa
+    def is_valido(list: Any) -> bool:
+        pass
